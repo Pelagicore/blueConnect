97472049c5af83d119946caa83ca82a3b9de96ec
Zeeshan Ali <zeenix@gmail.com>
2016-09-29 09:35:33 +0200

    Set copyright straight

--- blueconnect.cpp
+++ blueconnect.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2016, Pelagicore AB.
+ *
+ * Author: Zeeshan Ali <zeeshan.ali@pelagicore.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include "blueconnect.h"
 #include "iostream"
 
